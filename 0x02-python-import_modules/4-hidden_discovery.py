d1751d4ad63f0b43f0cf8d6a2c6618e5afe75c62
oussamaNAJIH <oussamanajih6@gmail.com>
2023-09-07 19:42:40 +0200

    4. Who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
 
     for str in dir(hidden_4):
-        if str[0]!= "_" and str[1] != "_":
+        if str[0] != "_" and str[1] != "_":
             print(str)

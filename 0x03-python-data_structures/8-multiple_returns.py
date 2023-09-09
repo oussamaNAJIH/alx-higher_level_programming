61c917d30c3e4b686f8bf51b06fb71a545c1dff6
oussamaNAJIH <oussamanajih6@gmail.com>
2023-09-09 15:45:28 +0200

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
         first_char = None
-    first_char = sentence[0]
+    else:
+        first_char = sentence[0]
     my_tuple = length, first_char
     return my_tuple

e7abe6e3a507904789446e537acdbd83df4e32e4
oussamaNAJIH <oussamanajih6@gmail.com>
2023-09-11 18:35:28 +0200

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,48 +2,42 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: pointer to the head of the list
  * Return: 1 if palindrome, 0 if not
  */
-
 int is_palindrome(listint_t **head)
 {
-size_t length = 0;
-int i = 0;
-size_t j;
-listint_t *current = *head;
-int *list;
-if (*head == NULL)
+if (head == NULL || *head == NULL)
 {
 return (1);
 }
-while (current != NULL)
+listint_t *slow = *head;
+listint_t *fast = *head;
+listint_t *previous = NULL;
+listint_t *link;
+while (fast != NULL && fast->next != NULL)
 {
-length++;
-current = current->next;
+fast = fast->next->next;
+link = slow->next;
+slow->next = previous;
+previous = slow;
+slow = link;
 }
-list = (int *)malloc(sizeof(int) * length);
-if (list == NULL)
+if (fast != NULL)
+{
+link = link->next;
+}
+while (slow != NULL && link != NULL)
+{
+if (slow->n != link->n)
 {
 return (0);
 }
-current = *head;
-while (current != NULL)
-{
-list[i] = current->n;
-current = current->next;
-i++;
+slow = slow->next;
+link = link->next;
 }
-for (j = 0; j < length / 2; j++)
-{
-if (list[j] != list[length - j - 1])
-{
-free(list);
-return (0);
-}
-}
-free(list);
 return (1);
 }

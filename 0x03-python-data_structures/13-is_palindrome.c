eca6ad98eb60832bca3cca7762be3bda0b4d864a
oussamaNAJIH <oussamanajih6@gmail.com>
2023-09-11 19:00:07 +0200

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,14 +11,14 @@
 
 int is_palindrome(listint_t **head)
 {
-if (head == NULL || *head == NULL)
-{
-return (1);
-}
 listint_t *slow = *head;
 listint_t *fast = *head;
 listint_t *previous = NULL;
 listint_t *link;
+if (head == NULL || *head == NULL)
+{
+return (1);
+}
 while (fast != NULL && fast->next != NULL)
 {
 fast = fast->next->next;

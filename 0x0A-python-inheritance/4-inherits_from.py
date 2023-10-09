8524b539358e77b1793988470a0041a965905113
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-09 19:46:29 +0200

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Only sub class of
+"""
+
+def inherits_from(obj, a_class):
+    """
+    Only sub class of
+    """
+    def inherits_from(obj, a_class):
+        return issubclass(type(obj), a_class)

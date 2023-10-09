e1fac86400112bc4d1fc18d03494cad495c7a827
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-09 21:03:52 +0200

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -12,5 +12,9 @@ class MyList(list):
         """
         Public instance method that prints the list, but sorted
         """
-        self.sort()
-        print(self)
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")

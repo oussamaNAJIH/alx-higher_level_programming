7ee4951f809d059b891fffe4a874742a68e66689
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-09 15:22:36 +0200

    inheritinh from List class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this module for inheriting from list
+"""
+
+
+class MyList(list):
+    """
+    this class inherits from list class
+    """
+    def print_sorted(self):
+        """
+        Public instance method that prints the list, but sorted
+        """
+        self.sort()
+        print(self)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")

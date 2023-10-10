9d2d9a1a89f7bbe416e8eb86acc6d60f943f0846
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-10 23:06:07 +0200

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,14 +2,16 @@
 """
 This module for Input/Output
 """
+
+
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
+myList = list(sys.argv[1:])
 try:
     existing_list = load_from_json_file("add_item.json")
 except Exception:
     existing_list = []
-myList = list(sys.argv[1:])
 existing_list.extend(myList)
 save_to_json_file(existing_list, "add_item.json")

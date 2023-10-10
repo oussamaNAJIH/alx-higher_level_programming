913ffd7f1f2f7285df2a708e7fd751207d3f3298
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-10 23:00:58 +0200

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,10 +3,10 @@
 This module for Input/Output
 """
 import sys
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
     try:
         existing_list = load_from_json_file("add_item.json")
     except FileNotFoundError:

037bc8e2204ef8b975f267703139da2187dc3fe4
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-15 12:27:56 +0200

    Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+"""
+This model for square class
+"""
+
+
+class Square(Rectangle):
+    """
+     class Square inherits from Rectangle
+    """
+    pass

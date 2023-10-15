c0acffb85b34ffcda7a7adb4b69afda2b4cd12c2
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-15 11:54:59 +0200

    override str function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,17 @@ class Rectangle(Base):
         returns the area value of the Rectangle instance
         """
         return self.__width * self.__height
+
+    def display(self):
+        """
+        prints in stdout the Rectangle instance with the character #
+        """
+        for i in range(self.__height):
+            print("#" * self.__width)
+
+    def __str__(self):
+        """
+        returns [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)

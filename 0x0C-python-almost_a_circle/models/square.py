188ca74f5ce3de854fad33aa529e426a088cbe67
oussamaNAJIH <oussamanajih6@gmail.com>
2023-10-15 12:50:36 +0200

    square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,18 @@ This model for square class
 
 class Square(Rectangle):
     """
-     class Square inherits from Rectangle
+    Class Square inherits from Rectangle.
     """
-    pass
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a new instance of the Square class.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns [Square] (<id>) <x>/<y> - <size>
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)

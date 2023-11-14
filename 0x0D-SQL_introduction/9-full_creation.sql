fd11add773fdb349ed26e6ac56dc797a4c54e281
oussamaNAJIH <oussamanajih6@gmail.com>
2023-11-14 18:40:33 +0100

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- second_table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table(name, score) VALUES('John', 10);
+INSERT INTO second_table(name, score) VALUES('Alex', 3);
+INSERT INTO second_table(name, score) VALUES('Bob', 14);
+INSERT INTO second_table(name, score) VALUES('George', 8);

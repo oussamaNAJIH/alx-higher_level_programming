aae58fed1caf8f731930627dac89f48913da8f80
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-11 11:32:29 +0100

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'nPython is cool', 'JavaScript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let index = 0; index < myArray.length; index++) {
   console.log(myArray[index]);
 }

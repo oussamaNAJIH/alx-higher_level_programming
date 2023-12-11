b05e4dc21e0385e5e738cfbfd1ed142fa4efaafa
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-11 12:14:37 +0100

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const size = parseInt(argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  if (size > 0) {
+    for (let i = 0; i < size; i++) {
+      let line = '';
+      for (let j = 0; j < size; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+}

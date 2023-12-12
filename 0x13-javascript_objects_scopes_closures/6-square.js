ac3faaee69b55a5182e0424d5277e54937ad2313
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-12 14:57:42 +0100

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ const SquareParent = require('./5-square');
 class Square extends SquareParent {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
+      super.print();
     } else {
       for (let i = 0; i < this.height; i++) {
         console.log('c'.repeat(this.width));

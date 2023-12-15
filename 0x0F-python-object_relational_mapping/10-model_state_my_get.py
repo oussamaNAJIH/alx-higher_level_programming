b426db3521004f46bf65045b5a3f35102e766be1
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-15 14:09:24 +0100

    Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    state_search = sys.argv[4]
+    Session = sessionmaker(engine)
+    session = Session()
+    state = session.query(State).filter(State.name == state_search).first()
+    print(state.id)
+    session.close()

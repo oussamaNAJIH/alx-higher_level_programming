405e7e16f014349e9517e0ae1eef204fa77c341c
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-15 13:11:05 +0100

    SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """
 script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa
 where name matches the argument
 """
-username = sys.argv[1]
-password = sys.argv[2]
-nameDb = sys.argv[3]
-searched = sys.argv[4]
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user=username, passwd=password,
-                       db=nameDb, charset="utf8")
-cur = conn.cursor()
-sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-cur.execute(sql_query, (searched, ))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    nameDb = sys.argv[3]
+    searched = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=username, passwd=password,
+                           db=nameDb, charset="utf8")
+    cur = conn.cursor()
+    sql_query = "SELECT * FROM states WHERE name " + \
+                "LIKE BINARY '%s' ORDER BY id ASC"
+    cur.execute(sql_query, (searched, ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

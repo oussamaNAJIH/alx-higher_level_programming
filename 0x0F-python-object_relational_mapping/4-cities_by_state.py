093e65047f341949101adfa47e4986b1a60a3701
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-15 13:15:50 +0100

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """
 script that lists all cities from the database hbtn_0e_4_usa
 """
-username = sys.argv[1]
-password = sys.argv[2]
-nameDb = sys.argv[3]
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user=username, passwd=password,
-                       db=nameDb, charset="utf8")
-cur = conn.cursor()
-sql_query = "SELECT cities.id, cities.name, states.name FROM cities LEFT " + \
-            "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
-cur.execute(sql_query)
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    nameDb = sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=username, passwd=password,
+                           db=nameDb, charset="utf8")
+    cur = conn.cursor()
+    sql_query = "SELECT cities.id, cities.name, states.name " +\
+                "FROM cities LEFT JOIN states " +\
+                "ON cities.state_id = states.id ORDER BY cities.id ASC"
+    cur.execute(sql_query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

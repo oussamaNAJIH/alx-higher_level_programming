1adf2c4f34cdcf258646379895931835c4570ca3
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-15 12:53:49 +0100

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """
 script that lists all states from the database hbtn_0e_0_usa
 """
+import sys
+import MySQLdb
 username = sys.argv[1]
 password = sys.argv[2]
 nameDb = sys.argv[3]

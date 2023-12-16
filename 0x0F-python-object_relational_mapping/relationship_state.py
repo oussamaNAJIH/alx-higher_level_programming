a9ae5722a93d2152cf391322e5ed6dda76c2ed0b
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-16 14:28:50 +0100

    City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+python file that contains the class definition of a State
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    states table and State class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")

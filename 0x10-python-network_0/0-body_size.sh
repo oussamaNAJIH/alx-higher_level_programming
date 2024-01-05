689018a1006a26637028e093a8fae9fb39910f6c
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-05 17:46:52 +0100

    size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a silent request to that URL using curl, and displays the size of the response body.
-curl -s -w "%{size_download}" -o /dev/null "$1" | { read size; echo "$size"; }
+# script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2

6b9f9f5328fe58a904909dae408a48c8ba936cad
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-05 18:13:07 +0100

    cURL headers

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a DELETE request to the URL passed as the first argument
-curl -s "$1" -X DELETE
+curl -X DELETE -s "$1"
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#A header variable X-School-User-Id must be sent with the value 98
+curl -s "$1" -H "X-School-User-Id: 98"

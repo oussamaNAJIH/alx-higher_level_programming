728c048ab01a126c04201efd9e520080a5560b97
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-05 17:42:18 +0100

    size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a silent request to that URL using curl, and displays the size of the response body.
-size=$(curl -s -w "%{size_download}" -o /dev/null "$1"); echo "$size"
+
+curl -s -w "%{size_download}" -o /dev/null "$1" | { read size; echo "$size"; }

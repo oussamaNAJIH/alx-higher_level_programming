d3ebdfb118d28517f405c8c0439ff31d1564ad08
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-07 12:40:09 +0100

    POST an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {"email": email}
-    r = requests.post(url, data = data)
+    r = requests.post(url, data=data)
     print(r.text)

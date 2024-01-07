996415ee637dc04499cf84ba24101dc19f541b5c
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-07 16:39:48 +0100

    auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that takes GitHub credentials (username and personal access token as password)
+Python script that takes GitHub credentials
 and uses the GitHub API to display your id
 """
 import requests
@@ -10,12 +10,9 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-
     auth = (username, password)
     url = "https://api.github.com/user"
-
     response = requests.get(url, auth=auth)
-    
     if response.status_code == 200:
         user_data = response.json()
         print(user_data.get("id"))

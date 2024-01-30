3b0700266e9a6a76a7c97df4e0ba90bc113ea112
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-30 17:52:02 +0100

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const path = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(path, content, function (error) {
+  if (error) {
+    console.log(error);
+  }
+});

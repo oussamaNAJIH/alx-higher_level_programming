7f593ccfae57f9a7089763463f21a929e6670586
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-30 18:40:33 +0100

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (!error) {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  } else {
+    console.log(error);
+  }
+});

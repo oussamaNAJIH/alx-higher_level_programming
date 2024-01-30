f128cdcee752130691cbce545ecba90125d21e20
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-30 17:47:02 +0100

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-const fs = require('node:fs');
-const path = process.argv[2];
-
-fs.readFile(path, 'utf-8', (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });

23597f5f4a1080585c748b8061e0df9edb32c451
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-31 16:51:50 +0100

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,32 @@
 #!/usr/bin/node
 const request = require('request');
+
 const movieId = parseInt(process.argv[2]);
 const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId + '/';
 
-request(movieUrl, function (error, response, body) {
+request(movieUrl, async function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
     const characters = data.characters;
+
+    // Using async/await to make requests in order
     for (let index = 0; index < characters.length; index++) {
-      request(characters[index], function (error, response, body) {
-        if (!error) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        } else {
-          console.log(error);
-        }
-      });
+      try {
+        const characterResponse = await new Promise((resolve, reject) => {
+          request(characters[index], function (error, response, body) {
+            if (!error) {
+              resolve(body);
+            } else {
+              reject(error);
+            }
+          });
+        });
+
+        const character = JSON.parse(characterResponse);
+        console.log(character.name);
+      } catch (error) {
+        console.log(error);
+      }
     }
   } else {
     console.log(error);

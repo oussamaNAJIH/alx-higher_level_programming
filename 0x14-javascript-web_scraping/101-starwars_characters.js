9ca446864f926fdbf0531305d3f2edddcf562cd6
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-31 16:32:01 +0100

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,17 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/';
-const movieId = process.argv[2];
+const movieId = parseInt(process.argv[2]);
 const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId + '/';
 
-request(url, function (error, response, body) {
-  if (!error) {
+request(movieUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
-    const people = data.results;
-    for (const character of people) {
-      if (character.films.includes(movieUrl)) {
-        console.log(character.name);
-      }
+    const characters = data.characters;
+    for (const characterUrl of characters) {
+      request(characterUrl, function (error, response, body) {
+        if (!error) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        } else {
+          console.log(error);
+        }
+      });
     }
   } else {
     console.log(error);
